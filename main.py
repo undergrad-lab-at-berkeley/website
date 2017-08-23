fd6f8f8533552c8eb8cb9fb82cb8bff318d8191b
Michael-Tu <tugan@berkeley.edu>
2017-08-22 23:17:32 -0700

    remove uncessary file and fix main entry point port

--- main.py
+++ main.py
@@ -69,7 +69,5 @@ def internal_server_error(e):
 
 #################### Main App #####################
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    app.debug = True
+    app.run()
 
