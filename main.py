f28602093cee871256283e94df0fa9e91fd886da
Undergraduate Lab At Berkeley <ulab@ocf.berkeley.edu>
2017-08-28 15:26:08 -0700

    Minor change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, url_for, redirect
 import content
+#from api import googleSheetAPI as sheetAPI
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
