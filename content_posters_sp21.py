f2e1d70d524bcd3a67a75670e20d27d9ba3ceb09
Yi J. Zhu <yi.zhu@berkeley.edu>
2021-04-26 01:14:55 -0700

    update poster site

--- content_posters_sp21.py
+++ content_posters_sp21.py
@@ -3,7 +3,10 @@ posters = {
         '[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview",
     },
     'physics': {
-        '[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview",
+        'Measuring Cosmic Distances using Gravitational Waves': "https://drive.google.com/file/d/1myT-xRH4oHdjK3rowAGYFgVYA-Wx7YNF/preview",
+        'Characterizing Exoplanet Habitability': 'https://drive.google.com/file/d/1MpvTY3GmaUm9tfYi4WPIzQPpLP4luvfb/preview',
+        'Categorizing Solar Flares with Machine Learning': 'https://drive.google.com/file/d/1PJ7x576jEb2WUaELUcUfEjW5SKEkkr4H/preview',
+        'An Exploration into Experimental Particle Physics': 'https://drive.google.com/file/d/1ZHCVqjliFNhXNEKLjf7t8r9P5HW0Zc8B/preview',
     },
     'bio': {
         '[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview"
