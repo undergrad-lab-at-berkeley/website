b2f5e0305290178fdac89902517776c1b0a532bb
Yi J. Zhu <yi.zhu@berkeley.edu>
2021-04-29 02:07:07 -0700

    posters unicode

--- content_posters_sp21.py
+++ content_posters_sp21.py
@@ -1,22 +1,22 @@
 posters = {
     'cogsci': {
-        '[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview",
+        u'[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview",
     },
     'physics': {
-        '1. Measuring Cosmic Distances using Gravitational Waves': "https://drive.google.com/file/d/1myT-xRH4oHdjK3rowAGYFgVYA-Wx7YNF/preview",
-        '2. Doppler Imaging of a Simulated Star':'https://drive.google.com/file/d/17XXGWVPc0m-oAVifBOlTol-exF1IGeE7/preview',
-        '3. Characterizing Exoplanet Habitability': 'https://drive.google.com/file/d/1MpvTY3GmaUm9tfYi4WPIzQPpLP4luvfb/preview',
-        '4. Categorizing Solar Flares with Machine Learning': 'https://drive.google.com/file/d/1PJ7x576jEb2WUaELUcUfEjW5SKEkkr4H/preview',
-        '5. An Exploration into Experimental Particle Physics': 'https://drive.google.com/file/d/1ZHCVqjliFNhXNEKLjf7t8r9P5HW0Zc8B/preview',
-        '6. Modeing and Mapping Terrestrial Gamma Ray Flashes': 'https://drive.google.com/file/d/1XJI4Ah7WSwa1dX1vXMmuWl1q2PdbGu-m/preview'
+        u'1. Measuring Cosmic Distances using Gravitational Waves': "https://drive.google.com/file/d/1myT-xRH4oHdjK3rowAGYFgVYA-Wx7YNF/preview",
+        u'2. Doppler Imaging of a Simulated Star':'https://drive.google.com/file/d/17XXGWVPc0m-oAVifBOlTol-exF1IGeE7/preview',
+        u'3. Characterizing Exoplanet Habitability': 'https://drive.google.com/file/d/1MpvTY3GmaUm9tfYi4WPIzQPpLP4luvfb/preview',
+        u'4. Categorizing Solar Flares with Machine Learning': 'https://drive.google.com/file/d/1PJ7x576jEb2WUaELUcUfEjW5SKEkkr4H/preview',
+        u'5. An Exploration into Experimental Particle Physics': 'https://drive.google.com/file/d/1ZHCVqjliFNhXNEKLjf7t8r9P5HW0Zc8B/preview',
+        u'6. Modeing and Mapping Terrestrial Gamma Ray Flashes': 'https://drive.google.com/file/d/1XJI4Ah7WSwa1dX1vXMmuWl1q2PdbGu-m/preview'
     },
     'bio': {
-        '1. Alzheimer’s Treatment: Silver Bullets vs. Dirty Drugs': "https://drive.google.com/file/d/1CR6sbqR28JWm_bw9GQJ3gcQLaZmf1l45/preview"
+        u'1. Alzheimer’s Treatment: Silver Bullets vs. Dirty Drugs': "https://drive.google.com/file/d/1CR6sbqR28JWm_bw9GQJ3gcQLaZmf1l45/preview"
     },
     'data': {
-        '[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview"
+        u'[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview"
     },
     'compbio': {
-        '[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview"
+        u'[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview"
     }
 }
