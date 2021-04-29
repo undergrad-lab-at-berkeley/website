106691a744ab986d95f0026f4e77adfdc2594cf8
Yi J. Zhu <yi.zhu@berkeley.edu>
2021-04-28 17:11:17 -0700

    poster site bug

--- content_posters_sp21.py
+++ content_posters_sp21.py
@@ -4,7 +4,7 @@ posters = {
     },
     'physics': {
         'Measuring Cosmic Distances using Gravitational Waves': "https://drive.google.com/file/d/1myT-xRH4oHdjK3rowAGYFgVYA-Wx7YNF/preview",
-        'Doppler Imaging of a Simulated Star':'https://drive.google.com/file/d/17XXGWVPc0m-oAVifBOlTol-exF1IGeE7/preview'
+        'Doppler Imaging of a Simulated Star':'https://drive.google.com/file/d/17XXGWVPc0m-oAVifBOlTol-exF1IGeE7/preview',
         'Characterizing Exoplanet Habitability': 'https://drive.google.com/file/d/1MpvTY3GmaUm9tfYi4WPIzQPpLP4luvfb/preview',
         'Categorizing Solar Flares with Machine Learning': 'https://drive.google.com/file/d/1PJ7x576jEb2WUaELUcUfEjW5SKEkkr4H/preview',
         'An Exploration into Experimental Particle Physics': 'https://drive.google.com/file/d/1ZHCVqjliFNhXNEKLjf7t8r9P5HW0Zc8B/preview',
