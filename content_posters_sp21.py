a7d64eb79c519f33be85920fc7e8211f7fdeb3ee
Yi J. Zhu <yi.zhu@berkeley.edu>
2021-05-01 01:09:12 -0700

    update posters

--- content_posters_sp21.py
+++ content_posters_sp21.py
@@ -2,31 +2,46 @@
 
 posters = {
     'cogsci': {
-        u"1. All in a Day's Laugh: A Replication and Extension of the Stress-Buffering Model of Positive Affect": "https://drive.google.com/file/d/17cC7nBGBVHSfXjHppDS1VPCpAYB_BToT/preview",
-        u"2. Visual Attention Intensifies Emotion: A Replication Study": 'https://drive.google.com/file/d/1e7sLVg-vJq-1svOv4C19nzAuluVB7p9_/preview',
-        u"3. Children's Use of Gender as a Social Cue": 'https://drive.google.com/file/d/1KSeV0qq4n9bdVjWDMWtbELHmqTTQ5b4J/preview',
-        u"4. Significant Group-Level Brain Activity During Trail-Making Test Performance": 'https://drive.google.com/file/d/1VI2S4t7G5flwXKyVXKuJ-bplFS76yjsG/preview',
-        u"5. A Reversal of Roles": 'https://drive.google.com/file/d/1CpP1MqIahdptaxkR3p_BZumnx9V3rVkH/preview',
-        u"6. Evidence for Cognitive Aging in Midlife Women: Study of Women’s Health Across the Nation - A Replication Study": 'https://drive.google.com/file/d/1o5xi5w3QCKCWBQrENJnW113XgywP1u_v/preview'
+        u"01. All in a Day's Laugh: A Replication and Extension of the Stress-Buffering Model of Positive Affect": "https://drive.google.com/file/d/17cC7nBGBVHSfXjHppDS1VPCpAYB_BToT/preview",
+        u"02. Visual Attention Intensifies Emotion: A Replication Study": 'https://drive.google.com/file/d/1e7sLVg-vJq-1svOv4C19nzAuluVB7p9_/preview',
+        u"03. Children's Use of Gender as a Social Cue": 'https://drive.google.com/file/d/1KSeV0qq4n9bdVjWDMWtbELHmqTTQ5b4J/preview',
+        u"04. Significant Group-Level Brain Activity During Trail-Making Test Performance": 'https://drive.google.com/file/d/1VI2S4t7G5flwXKyVXKuJ-bplFS76yjsG/preview',
+        u"05. A Reversal of Roles": 'https://drive.google.com/file/d/1CpP1MqIahdptaxkR3p_BZumnx9V3rVkH/preview',
+        u"06. Evidence for Cognitive Aging in Midlife Women: Study of Women’s Health Across the Nation - A Replication Study": 'https://drive.google.com/file/d/1o5xi5w3QCKCWBQrENJnW113XgywP1u_v/preview',
+        u"07. Distinct Representations of Subtraction and Multiplication in the Neural Systems for Numerosity and Language - A Replication Study": 'https://drive.google.com/file/d/1Xlh5UeLaNNUFCAx_13YDLHIuIapr--o0/preview',
+        u'08. Exploration of Subthreshold Beta Amyloid Levels and Effects on Cognitive Function': 'https://drive.google.com/file/d/1aIhf8hDwP0mqbzKgvTtY3YPW1xbTcE75/preview',
+        u'09. Invariant Representations of Mass in the Human Brain & Its Effects on Inferences of Physical Collisions': "https://drive.google.com/file/d/1dvNmcpMzCFe82iMdc90YJ3qhm31NN_cG/preview",
+        u'10. Experiential stress and physiological stress: Implication of coherence': "https://drive.google.com/file/d/1xkwiL2FHbtBtTf_J6R8Ugggij2RpYTum/preview",
+        u"11. ‘Fairness Informs Social Decision Making in Infancy’ - A Replication": "https://drive.google.com/file/d/1t4bASn3LGIjxfucHPpnuc0vwG_cqg2VS/preview",
+        u'12. Pelvic Irradiation-Induced Fatigue Extension': "https://drive.google.com/file/d/1BNLBvZ-kz-gV_tSRyV52fPgs9_HfB8sQ/preview",
+        u"13. Lingering Negative Affect in Response to Daily Stressors in relation with Physical Health Years Later: A Longitudinal Replication": "https://drive.google.com/file/d/1gFrIZrNkkZTaGr9sv5U4nxUi2LADZul-/preview",
+        u'14. Statistical Replication of Pomper & Saffran (2016)': "https://drive.google.com/file/d/1ghzsGnfjepi8s0TURWY88BQMrEW1mJYL/preview",
     },
     'physics': {
         u'1. Measuring Cosmic Distances using Gravitational Waves': "https://drive.google.com/file/d/1myT-xRH4oHdjK3rowAGYFgVYA-Wx7YNF/preview",
         u'2. Doppler Imaging of a Simulated Star':'https://drive.google.com/file/d/17XXGWVPc0m-oAVifBOlTol-exF1IGeE7/preview',
         u'3. Characterizing Exoplanet Habitability': 'https://drive.google.com/file/d/1MpvTY3GmaUm9tfYi4WPIzQPpLP4luvfb/preview',
         u'4. Categorizing Solar Flares with Machine Learning': 'https://drive.google.com/file/d/1ba_pkRQrxLCTBM5zfLfJ1Fn_VXxwrD2D/preview',
-        u'5. An Exploration into Experimental Particle Physics': 'https://drive.google.com/file/d/1ZHCVqjliFNhXNEKLjf7t8r9P5HW0Zc8B/preview',
-        u'6. Modeling and Mapping Terrestrial Gamma Ray Flashes': 'https://drive.google.com/file/d/1XJI4Ah7WSwa1dX1vXMmuWl1q2PdbGu-m/preview'
+        u"5. Determining Hubble's Constant From Time Delays in Lensed Quasars ": 'https://drive.google.com/file/d/1NLldjv7BRkUK03_FV060mEJ50hTdk1Me/preview',
+        u'6. An Exploration into Experimental Particle Physics': 'https://drive.google.com/file/d/1ZHCVqjliFNhXNEKLjf7t8r9P5HW0Zc8B/preview',
+        u'7. Modeling and Mapping Terrestrial Gamma Ray Flashes': 'https://drive.google.com/file/d/1XJI4Ah7WSwa1dX1vXMmuWl1q2PdbGu-m/preview'
     },
     'bio': {
         u"1. Alzheimer's Treatment: Silver Bullets vs. Dirty Drugs": "https://drive.google.com/file/d/1M2U7hGUzOjac5BnUkVUraotmzMK-Wd56/preview",
         u"2. Targets of Triple-Negative Breast Cancer and Anti-Tumor Activity": 'https://drive.google.com/file/d/1Xu00N5tpbO3VOiNVj7xf2UQ_Ky3LgMGX/preview',
-
+        u"3. An Analysis of Environmental Factors Impacting Health": "https://drive.google.com/file/d/1CFKGXUOxOAcwGZ961bfZXr7Vt94teGB9/preview",
+        u"4. A Study on COVID-19 Travel Behavior and Transmission": 'https://drive.google.com/file/d/1swVim1aFr2_Z4WMsTsz-ZgQVcWYP7R9f/preview',
     },
     'data': {
-        u'[TBD]': "https://drive.google.com/file/d/18sC6FdmkffQk4k55II8otncqS7k-FZ-6/preview"
+        u"1. Using Classification and Regression to Predict and Model Mortality Rates": "https://drive.google.com/file/d/13gCWtao9Oiv3tYkDziLxrdQhvkVCY8wU/preview",
+        u"2. Deeper Dive into the NBA during COVID-19": 'https://drive.google.com/file/d/1g2ZfXSmmhSxlk7H32lpuluLMcJoAZ0yU/preview',
+        u'3. Wildfire Detection': 'https://drive.google.com/file/d/1UW19aKpkubgdk1CRmQEtLmYKbW_BaJcg/preview',
+        u'4. Developing a Natural Method to Measure the  Complexity of Language and Applications to Color Naming': "https://drive.google.com/file/d/1dMxS-ovmVcXcH_TfZ5d4EroB2J3WJ5zs/preview",
     },
     'compbio': {
         u"1. Study of Processed Pseudogenes in Three Types of Female Specific Cancers": 'https://drive.google.com/file/d/1s4kXdvNA67GspjmVrN8VZVh8Hqzt-Fs1/preview',
-
+        u"2. Examining Variants Associated With Huntington’s Disease and Treatment Through Gene-Editing Techniques": 'https://drive.google.com/file/d/1iO0FyBg1EE8V-ISsUSn7MOYL4qe3ZkFJ/preview',
+        u'3. Determination of the Best Peanut Proteins Candidates for Gene Editing': 'https://drive.google.com/file/d/1KPlgG8I8smds5Yih9GTd_rnF161-ULqQ/preview',
+        u'4. The connection between synonymous variants of CDH23 and Alzheimer’s disease': "https://drive.google.com/file/d/18jQsVsCvTv35ohBl4Xo1TzadMvIzJcwy/preview",
     }
 }
