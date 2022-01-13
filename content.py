28b03ed96ce6b3931a1fdefef42dfe73f3f72af7
Yi J. Zhu <yi.zhu@berkeley.edu>
2022-01-13 15:20:21 -0800

    correct file path

--- content.py
+++ content.py
@@ -13,7 +13,10 @@ import csv
 
 # physics lab read former members 
 # this is all super janky
-file = csv.DictReader(open('/static/doc/physics_former.csv', 'r'))
+try:
+    file = csv.DictReader(open('static/doc/physics_former.csv', 'r'))
+except Exception:
+    file = csv.DictReader(open('/static/doc/physics_former.csv', 'r'))
 
 former_name = []
 former_position = []
