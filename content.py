653a8cbcbffdddd49aa19b2a013e2982e746673b
Hank Wang <hankwang001@berkeley.edu>
2022-08-30 17:59:03 -0700

    Merge Issues

--- content.py
+++ content.py
@@ -283,7 +283,7 @@ labs = {
             "full_name": u"Health Sciences",
             "navbar": u"Health Sciences",
             "status": "Active",
-            "members": ["Shrina Pandey", "Meeseo Lee","Ting Guo", "Ishna Pandey", "Anisha Chandy", "Jenny Wu", "Aishwarya Ballapuram", "Andrew Koh", "Tanirika Singh", "Jean Ye", "Tiffany Yeh", "Serena Chi", "Hank Wang", "Nancy Nguyen", "Albert Wu"],
+            "members": ["Shrina Pandey", "Meeseo Lee","Ting Guo", "Ishna Pandey", "Anisha Chandy", "Jenny Wu", "Aishwarya Ballapuram", "Andrew Koh", "Tanirika Singh", "Jean Ye", "Tiffany Yeh", "Serena Chi", "Hank Wang", "Nancy Nguyen", "Albbert Wu"],
             "content": {
                 "overview": {
                     "title": "Lab Overview Coming Soon!",
@@ -292,8 +292,8 @@ labs = {
                 "join": {
                     "title": "Want to join us?",
                     "text": u"""
-                            We are currently taking applications for the 2022-2023 school year! <br><br>
-                            The deadline to <br> <a href='https://docs.google.com/forms/d/e/1FAIpQLSd6VArk0S8e38rJmDDZcsOcxCIpGoF7r6267atSa6Bp5M5aKw/viewform'>apply</a> to be a mentee is September 30th, 2022 @ 11:59PM! <br><br>
+                            We are currently taking applications for the 2022-2023 school year! <br/>
+                            The deadline to <br> <a href='https://docs.google.com/forms/d/e/1FAIpQLSd6VArk0S8e38rJmDDZcsOcxCIpGoF7r6267atSa6Bp5M5aKw/viewform'>apply</a> to be a mentee is September 30th, 2022 @ 11:59PM! <br/>
                             The deadline to <br> <a href='https://docs.google.com/forms/d/e/1FAIpQLSdlabFQrdyUplBJGtw55VHNPAL7wfv8JPMS0w3lPg67Cfchjg/viewform'>apply</a> to be a mentor is September 23rd, 2022 @ 11:59PM! <br><br>
                             """
                 },
